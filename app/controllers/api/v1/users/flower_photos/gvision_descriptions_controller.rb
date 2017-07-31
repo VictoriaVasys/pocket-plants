5f1527429ed3cd1766e989d55fd45ccbd86931d4
VictoriaVasys <vvasys@gmail.com>
2017-07-30 18:40:28 -0600

    Begin index page for user/fave/fps

--- app/controllers/api/v1/users/flower_photos/gvision_descriptions_controller.rb
+++ app/controllers/api/v1/users/flower_photos/gvision_descriptions_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::Users::FlowerPhotos::GvisionDescriptionsController < Api::V1::Users::BaseController
   # skip_before_action :verify_authenticity_token
+  skip_before_action :authorize_user, only: [:show, :index]
   
   def index
     @gvision_descriptions = flower_photo.gvision_descriptions
@@ -22,15 +23,15 @@ class Api::V1::Users::FlowerPhotos::GvisionDescriptionsController < Api::V1::Use
     end
   end
   
-  def update
-    @gvision_description = flower_photo.gvision_descriptions.find(params[:id])
-    @gvision_description.update(name: gvision_description_params[:name])
-    if @gvision_description.save
-      render :show, status: 200
-    else
-      render file: "public/422.html",  status: 422
-    end
-  end
+  # def update
+  #   @gvision_description = flower_photo.gvision_descriptions.find(params[:id])
+  #   @gvision_description.update(name: gvision_description_params[:name])
+  #   if @gvision_description.save
+  #     render :show, status: 200
+  #   else
+  #     render file: "public/422.html",  status: 422
+  #   end
+  # end
   
   def destroy
     @gvision_description = flower_photo.gvision_descriptions.find(params[:id])
