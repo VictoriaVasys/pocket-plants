9f53be89a7c8a690d2071eac873ddd55e27eb893
VictoriaVasys <vvasys@gmail.com>
2017-08-01 07:22:36 -0600

    Update tests for flower-photo-show

--- app/assets/javascripts/components/flower_photo/_gvision_descriptions_container.js.jsx
+++ app/assets/javascripts/components/flower_photo/_gvision_descriptions_container.js.jsx
@@ -3,7 +3,7 @@ var GvisionDescriptionsContainer = React.createClass({
   render() {
     var gvision_descriptions = this.props.gvision_descriptions.map((gvision_description)=> {
       return(
-        <div className='row gvision_description' key={gvision_description.id}>
+        <div className='row gvision-description' key={gvision_description.id}>
           <div className='col-md-8 col-md-offset-2'>
             <h5 tabIndex='0'>{gvision_description.name}</h5>
           </div>
@@ -12,7 +12,7 @@ var GvisionDescriptionsContainer = React.createClass({
     })
     
     return(
-      <div className="gvision_descriptions">
+      <div className="gvision-descriptions">
         {gvision_descriptions}
       </div>
     )
--- spec/features/flower_photos/visitor_can_see_a_flower_photo_spec.rb
+++ spec/features/flower_photos/visitor_can_see_a_flower_photo_spec.rb
@@ -10,39 +10,26 @@ feature "An unauthenticated user", :js => :true do
   scenario "can view their favorite flower photos" do
     visit "/#{flower_photo.user.username}/#{flower_photo.sanitized_name}"
     
+    expect(page).to have_content(flower_photo.assigned_name)
     within (".flower-photo-show") do
-      expect(page).to have_content(flower_photo.assigned_name)
-      expect(page).to have_content("#{flower_photo.plant_family.taxonomic_name} family")
+      expect(page).to have_content("#{flower_photo.plant_family.taxonomic_name}")
       expect(page).to have_css("img[src*='#{flower_photo.storage_url}']")
       expect(page).to have_content("Photographer: #{flower_photo.user.username}")
+    end
       
-      within('.flower-photo-favorites') do
-        expect(page).to have_content("#{flower_photo.favorites.count}")
-      end
-      
-      within('.gvision-descriptions') do
-        expect(page).to have_content("#{flower_photo.gvision_descriptions.first.name}")
-        expect(page).to have_content("#{flower_photo.gvision_descriptions.second.name}")
-        expect(page).to have_content("#{flower_photo.gvision_descriptions.last.name}")
-        
-        within('.gvision-descriptions-favorites-count') do
-          expect(page).to have_content("#{flower_photo.gvision_descriptions.first.favorites.count}")
-          expect(page).to have_content("#{flower_photo.gvision_descriptions.second.favorites.count}")
-          expect(page).to have_content("#{flower_photo.gvision_descriptions.last.favorites.count}")
-        end
-      end
-      
-      within('.comments') do
-        expect(page).to have_content("#{flower_photo.comments.first.name}")
-        expect(page).to have_content("#{flower_photo.comments.second.name}")
-        expect(page).to have_content("#{flower_photo.comments.last.name}")
-        
-        within('.comments-favorites-count') do
-          expect(page).to have_content("#{flower_photo.comments.first.favorites.count}")
-          expect(page).to have_content("#{flower_photo.comments.second.favorites.count}")
-          expect(page).to have_content("#{flower_photo.comments.last.favorites.count}")
-        end
-      end
+    within('.gvision-descriptions') do
+      expect(page).to have_content("#{flower_photo.gvision_descriptions.first.name}")
+      expect(page).to have_content("#{flower_photo.gvision_descriptions.second.name}")
+      expect(page).to have_content("#{flower_photo.gvision_descriptions.last.name}")
+    end
+    
+    within('.comments') do
+      expect(page).to have_content("#{flower_photo.comments.first.user.username}")
+      expect(page).to have_content("#{flower_photo.comments.first.body}")
+      expect(page).to have_content("#{flower_photo.comments.second.user.username}")
+      expect(page).to have_content("#{flower_photo.comments.second.body}")
+      expect(page).to have_content("#{flower_photo.comments.last.user.username}")
+      expect(page).to have_content("#{flower_photo.comments.last.body}")
     end
   end
 
